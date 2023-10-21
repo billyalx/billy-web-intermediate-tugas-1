f48dcfb422d68e11fed7d51d6ce5ab0c69d65658
Billy Alexander <billyalexndr@gmail.com>
2023-10-21 17:48:47 +0700

    on progress task 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+'use client';
 export default function Home() {
   return (
     <>
       <h1>Welcome to My Webpage</h1>
-      <p>owner: Billy Alexander</p>
+      <p>Host: {process.env.NEXT_PUBLIC_DB_HOST}</p>
+      <p>Username: {process.env.NEXT_PUBLIC_DB_USER}</p>
+      <a href="https://billy-alexander.vercel.app/">My Portofilio</a>
     </>
   );
 }
