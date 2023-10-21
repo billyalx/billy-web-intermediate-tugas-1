f38a5ff84cf4b6ae5fe65c4c1428f34dc2f4b6d4
Billy Alexander <billyalexndr@gmail.com>
2023-10-21 17:48:15 +0700

    on progress task 2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000, // cache halaman web akan bertahan selama 25 detk
+    pagesBufferLength: 2, // max cahce halaman sebanyak 2
+  },
+  experimental: {
+    optimizePackageImports: ['react-bootstrap'], // akan mengoptimalkan import react-bootstrap
+  },
+};
